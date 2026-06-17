d96197e2358f5c95244226ac05074d440c6b22c2
SandipBajracharya <sndp106.sb@gmail.com>
2026-06-17 17:55:29 +0545

    perf(OUT-3879): increase max duration to 5 mins for assembly webhook api

--- src/app/api/webhook/assembly/route.ts
+++ src/app/api/webhook/assembly/route.ts
@@ -1,4 +1,6 @@
 import { handleWebhookEvent } from '@/features/webhook/assembly/api/webhook.controller'
 import { withErrorHandler } from '@/utils/withErrorHandler'
 
+export const maxDuration = 300 // 5 mins
+
 export const POST = withErrorHandler(handleWebhookEvent)
